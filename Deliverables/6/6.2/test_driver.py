356b1459dfb9cbd23dc9e63c205b8af42d10be68
benw1672 <benw1672@gmail.com>
2019-11-06 15:06:30 -0600

    Trying to fix 6.2 using jsonpickle

--- Deliverables/6/6.2/test_driver.py
+++ Deliverables/6/6.2/test_driver.py
@@ -1,5 +1,5 @@
 # Import nonlocal dependencies.
-import json, os, sys, typing
+import json, os, sys, typing, copy
 import jsonpickle
 
 # Import local dependencies.
@@ -22,20 +22,20 @@ def main():
         # If the move is a pass
         if i == 0:
             results.append(rf.register_black_player(json_element))
-            #print(rf.register_black_player(json_element))
+            print(rf.register_black_player(json_element))
             continue
         elif i == 1:
             results.append(rf.register_white_player(json_element))
-            #print(rf.register_white_player(json_element))
+            print(rf.register_white_player(json_element))
             continue
         elif i == 2:
-            res = rf.board_history
+            res = copy.deepcopy(rf.board_history)
             results.append(res)
-            #map(lambda board: board.pretty_print(), res)
-            #print("[")
-            #for b in res:
-            #    b.pretty_print()
-            #print("]")
+            print("[")
+            for b in res:
+               print(b)
+            print("]")
+            print()
         if rf.is_game_ended:
             break
         else:
@@ -43,17 +43,17 @@ def main():
                 play_or_pass = PASS
             else:
                 play_or_pass = Point.from_str(json_element)
-            res = rf.play_point(play_or_pass)
+            res = copy.deepcopy(rf.play_point(play_or_pass))
             if isinstance(res[0], Board):
                 results.append(res)
-                #print("[")
-                #for b in res:
-                #    b.pretty_print()
-                #print("]")
-                #map(lambda board: board.pretty_print(), res)
+                print("[")
+                for b in res:
+                   print(b)
+                print("]")
+                print()
             else:
                 results.append(res)
-                #print(res)
+                print(res)
     if rf.is_game_ended == False:
         results.pop()
 
