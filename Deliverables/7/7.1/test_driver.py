92c1bbed84f4d8b82685b14a7599136d1c642d3c
Wung Jae Lee <wungjaelee@Wungs-MBP.attlocal.net>
2019-11-12 16:50:21 -0600

    run 7.1

--- Deliverables/7/7.1/test_driver.py
+++ Deliverables/7/7.1/test_driver.py
@@ -1,13 +1,11 @@
 # Import nonlocal dependencies.
-import json, os, sys, typing, copy
-import jsonpickle
+import json, os, sys, typing
 
 # Import local dependencies.
-import player, strategies, referee
-from point import Point
+from player import handle_player, PlayerStateProxy, PlayerContractProxy, Player
+import strategies
 from board import Board
 from constants import *
-from referee import Referee
 
 
 def main():
@@ -15,49 +13,19 @@ def main():
     txt = sys.stdin.read().rstrip()
     json_elements = txt2json(txt)
 
-    rf = Referee()
-    results = []
-    res_print = []
-    for i, json_element in enumerate(json_elements):
-        # If the move is a pass
-        if i == 0:
-            results.append(rf.register_black_player(json_element))
-            # print(rf.register_black_player(json_element))
-            continue
-        elif i == 1:
-            results.append(rf.register_white_player(json_element))
-            # print(rf.register_white_player(json_element))
-            continue
-        elif i == 2:
-            res = copy.deepcopy(rf.board_history)
-            results.append(res)
-            # print("[")
-            # for b in res:
-            #    print(b)
-            # print("]")
-            # print()
-        if rf.is_game_ended:
-            break
-        else:
-            if json_element == PASS:
-                play_or_pass = PASS
-            else:
-                play_or_pass = Point.from_str(json_element)
-            res = copy.deepcopy(rf.play_point(play_or_pass))
-            if isinstance(res[0], Board):
-                results.append(res)
-                # print("[")
-                # for b in res:
-                #    print(b)
-                # print("]")
-                # print()
-            else:
-                results.append(res)
-                # print(res)
-    if rf.is_game_ended == False:
-        results.pop()
+    # Instantiate dependencies.
+    player = Player(strategy=strategies.PrioritizeCaptureStrategy())
+    player_with_contract_state = PlayerStateProxy(PlayerContractProxy(player))
 
-    print(jsonpickle.encode(results, unpicklable=False))
+    # Handle each input and collect the results.
+    results = []
+    for json_element in json_elements:
+        res = handle_player(player_with_contract_state, json_element)
+        if res:
+            results.append(res)
+        if res == GO_HAS_GONE_CRAZY:
+            break
+    json.dump(results, sys.stdout)
 
 
 def txt2json(content: str) -> list:
